2925d85378387c5d11d9eb47431df52060b69cb1
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-02-12 16:37:08 +0800

    1

--- public/js/plugin_reference_id.js
+++ public/js/plugin_reference_id.js
@@ -8,8 +8,8 @@
         Plugin_refid.generateLocal = function (identifier) {
 
             var date    = new Date();
-            var month   = date.getMonth() + 1;
-            var day     = date.getDate();
+            var month   = date.getMonth();
+            var day     = date.getDay();
             var year    = date.getFullYear();
             var hour    = date.getHours();
             var minute  = date.getMinutes();
