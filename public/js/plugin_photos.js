597e95d3fbe672a20176403ed44ed041b5eaab9c
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-02-16 21:54:52 +0800

    Fixed

--- public/js/plugin_photos.js
+++ public/js/plugin_photos.js
@@ -6,13 +6,6 @@
         var Plugin_photos   = {};
 		var env_api 		=  Plugin_config_file.projects()['env_api_multi_purpose'];
 
-        if(env == 'live') {
-			env_api = env_live;
-		}
-		else {
-			env_api = env_local;
-		}
-
         Plugin_photos.saveInfoTemp = function (photo_refid, filepath, tagged, user_refid, callback) {
             $.get( env_api + "api/plugin_photo/saveInfoTemp?reference_id="+ photo_refid +"&filepath="+ filepath +"&tagged="+ tagged +"&user_refid=" + user_refid, function (response) {
 				callback(response);
