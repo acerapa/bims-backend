fe648ec547bd37eae8c32266318e6891d8d00f97
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-02-21 13:46:38 +0800

    Initial Blog plugin

---
+++ app/Http/Controllers/plugin_blog/Get.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\plugin_blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Get extends Controller
+{
+    //
+}
