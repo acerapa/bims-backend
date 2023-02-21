34459f50b2f0060d661ac7c9069ede12a9e46d57
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-02-22 03:33:22 +0800

    JS Code added

--- public/js/plugin_blog.js
+++ public/js/plugin_blog.js
@@ -6,6 +6,12 @@
         var Plugin_blog     = {};
         var env_api 		=  Plugin_config_file.projects()['env_api_multi_purpose'];
 
+		Plugin_blog.getSingle = function (blog_refid, callback) {
+            $.get( env_api + "api/plugin_blog/getSingle?blog_refid="+ blog_refid, function (response) {
+				callback(response);
+			});
+        }
+
         Plugin_blog.getPaginate = function (category_refid, page, callback) {
             $.get( env_api + "api/plugin_blog/getPaginate?category_refid="+ category_refid +"&page=" + page, function (response) {
 				callback(response);
