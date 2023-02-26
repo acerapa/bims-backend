982e1f054b463c28cc1d80daead5437189413f8c
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-02-26 22:51:00 +0800

    Review updates

--- public/js/plugin_review.js
+++ public/js/plugin_review.js
@@ -4,7 +4,7 @@
 	function PluginReview() {
 
         var Plugin_review   = {};
-		var env_api 		=  Plugin_config_file.projects()['env_api_multi_purpose'];
+		var env_api 		= Plugin_config_file.projects()['env_api_multi_purpose'];
 
         Plugin_review.create = function (reference_id, tag_primary, name, photo, score, hightlight, comment, other, taglist, callback) {
             $.get( env_api + "api/plugin_review/create?reference_id="+ reference_id +"&tag_primary="+ tag_primary +"&name="+ name +"&photo="+ photo +"&score="+ score +"&hightlight="+ hightlight +"&comment=" + comment + "&other="+ other +"&taglist=" + taglist, function (response) {
@@ -18,7 +18,7 @@
 			});
 		};
 
-		Plugin_review.getScoreRecalculate = function (tag_refid, tag_table, tag_whereClm, tag_whereVal) {
+		Plugin_review.getScoreRecalculate = function (tag_refid, tag_table, tag_whereClm, tag_whereVal, callback) {
 			$.get( env_api + "api/plugin_review/calculate?tag_refid="+ tag_refid +"&table="+ tag_table +"&whereClm="+ tag_whereClm +"&whereVal="+tag_whereVal, function (response) {
 				callback(response);
 			});
