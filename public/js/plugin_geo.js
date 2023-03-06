c1f68ad60563620252c1d36816c440e0a687a593
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-06 22:06:59 +0800

    OK

--- public/js/plugin_geo.js
+++ public/js/plugin_geo.js
@@ -4,42 +4,49 @@
 	function PluginGeo() {
         var Plugin_geo   = {};
 
-        Plugin_geo.allRegion = function (callback) {
+        Plugin_geo.allRegion = function (fetch_local) {
             
         };
 
-        Plugin_geo.allProvince = function (region_code, callback) {
+        Plugin_geo.allProvince = function (fetch_local, region_code, callback) {
             
         };
 
-        Plugin_geo.allCity = function (province_code, callback) {
+        Plugin_geo.allCity = function (fetch_local, province_code, callback) {
             
         };
 
-        Plugin_geo.allBarangay = function (city_code, callback) {
+        Plugin_geo.allBarangay = function (fetch_local, city_code, callback) {
             
         };
 
-        Plugin_geo.allActiveRegion = function (callback) {
+        Plugin_geo.allActiveRegion = function (fetch_local, callback) {
             
         };
 
-        Plugin_geo.allActiveProvince = function (region_code, callback) {
+        Plugin_geo.allActiveProvince = function (fetch_local, region_code, callback) {
             
         };
 
-        Plugin_geo.allActiveCity = function (province_code, callback) {
+        Plugin_geo.allActiveCity = function (fetch_local, province_code, callback) {
             
         };
 
-        Plugin_geo.allActiveBarangay = function (city_code, callback) {
+        Plugin_geo.allActiveBarangay = function (fetch_local, city_code, callback) {
             
         };
 
-        Plugin_geo.allActiveCityWithProvice = function (callback) {
-            $.get( Plugin_config_file.projects()['env_api'] + "api/plugin_geography/allActiveCityWithProvice", function (response) {
-                callback(response);
-            });
+        Plugin_geo.allActiveCityWithProvice = function (fetch_local, callback) {
+            const local = localStorage.getItem("allActiveCityWithProvice");
+            if(local && fetch_local) {
+                callback(JSON.parse(local));
+            }
+            else {
+                $.get( Plugin_config_file.projects()['env_api'] + "api/plugin_geography/allActiveCityWithProvice", function (response) {
+                    localStorage.setItem("allActiveCityWithProvice", JSON.stringify(response));
+                    callback(response);
+                });
+            }
         };
 
         return Plugin_geo;
