aee06dc403de122422d275427379c1c66a96de3f
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-20 23:29:55 +0800

    1

---
+++ app/Http/Controllers/plugin_delete_recyclable/Delete.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\plugin_delete_recyclable;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Delete extends Controller
+{
+    //
+}
