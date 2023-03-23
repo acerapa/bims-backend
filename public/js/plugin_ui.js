e45f10ae97f71c9cd76fef5207198a1136c3f835
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-23 10:25:05 +0800

    populateDropdown

--- public/js/plugin_ui.js
+++ public/js/plugin_ui.js
@@ -4,6 +4,19 @@
 	function PluginUI() {
         var Plugin_ui   = {};
 
+        Plugin_ui.populateDropdown = function (elem, options, defaultVal) {
+            $(elem).html('');
+            $(elem).append(`<option value='0'>Select</option>`);
+            for(let i = 0; i < options.length; i++) {
+                if(options[i]['value'] == defaultVal) {
+                    $(elem).append(`<option value='`+ options[i]['value'] +`' selected>`+ options[i]['label'] +`</option>`);
+                }
+                else {
+                    $(elem).append(`<option value='`+ options[i]['value'] +`'>`+ options[i]['label'] +`</option>`);
+                }
+            }
+        };
+
         Plugin_ui.buttonUploading = function (isUploading, elem, label) {
             if(isUploading) {
                 $(elem).prop("disabled", true).css({'cursor':'not-allowed'});
