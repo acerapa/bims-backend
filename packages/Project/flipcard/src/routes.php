ea2f8ac20960b8c7092198389fd97c0817abe016
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-26 15:32:24 +0800

    First release

--- packages/Project/flipcard/src/routes.php
+++ packages/Project/flipcard/src/routes.php
@@ -4,7 +4,7 @@
 
 
 Route::group(['prefix' => 'flipcard'], function () {
-    Route::get("set",[Project\Flipcard\FlipcardController::class, 'set']);
+    Route::get("submit",[Project\Flipcard\FlipcardController::class, 'submit']);
 });
 
 
