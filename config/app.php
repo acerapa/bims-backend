8bed46ca0bee6b954af8719f505e4b2a165a31d3
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-30 10:28:31 +0800

    Some adeded

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Project\Flipcard\FlipcardServiceProvider::class,
+        Project\Deanlief\DeanliefServiceProvider::class,
 
     ],
 
