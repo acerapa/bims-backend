96293a65c6fba7f78fbfb10e55c2bf73b2e99747
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-30 13:23:07 +0800

    WhereIn Query Fixed

--- packages/Project/deanlief/src/routes.php
+++ packages/Project/deanlief/src/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * 
+ * deanlief/variables
+ */
+
 Route::group(['prefix' => 'deanlief'], function () {
     Route::get("delete_listing/{listing_refid}",[Project\Deanlief\DeanliefController::class, 'delete_listing']);
     Route::get("search_listing",[Project\Deanlief\DeanliefController::class, 'search_listing']);
