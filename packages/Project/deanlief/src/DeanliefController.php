9699a665faa5ced08d489c85a0f419304e64207d
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-03-30 12:24:09 +0800

    Deanlief Variable moved to package

--- packages/Project/deanlief/src/DeanliefController.php
+++ packages/Project/deanlief/src/DeanliefController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Project\Deanlief;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -60,5 +60,140 @@ class DeanliefController extends Controller
         return DB::table("listing")
         ->where([$market, $property_type, $city_code])
         ->paginate(12);
+    }
+
+    public static function variables() {
+        return [
+            "listing_property_aminities"    => [
+                "form"  => [
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Balcony",
+                        "value"         => false,
+                    ],
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Terrace",
+                        "value"         => false,
+                    ],
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Air Conditioning",
+                        "value"         => false,
+                    ],
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Built-In Wardrobes",
+                        "value"         => false,
+                    ],
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Swimming Pool",
+                        "value"         => false,
+                    ],
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Garden",
+                        "value"         => false,
+                    ],
+                    [
+                        "type"          => "checkbox",
+                        "label"         => "Parking Space",
+                        "value"         => false,
+                    ]
+                ]
+            ],
+            "listing_property_type" => [
+                [
+                    "code"          => "APRT",
+                    "label"         => "Apartment",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "CNDM",
+                    "label"         => "Condominium",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "DPLX",
+                    "label"         => "Duplex",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "FRRT",
+                    "label"         => "For Rent",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "HSLT",
+                    "label"         => "House and Lot",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "LAND",
+                    "label"         => "Land",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "LTPJ",
+                    "label"         => "Lot Only Projects",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "OFSP",
+                    "label"         => "Office Space",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "TWHS",
+                    "label"         => "Townhouse",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "WRHS",
+                    "label"         => "Warehouse",
+                    "enabled"       => true
+                ],
+                [
+                    "code"          => "VLLA",
+                    "label"         => "Villa",
+                    "enabled"       => true
+                ]
+            ],
+            "listing_market"    => [
+                "FRSL"         => "For Sale",
+                "FRRT"         => "For Rent"
+            ],
+            "user_position" => [
+                "RGSR"         => [
+                    "label"         => "Regular User",
+                    "enabled"       => true
+                ],
+                "PRDT"         => [
+                    "label"         => "President",
+                    "enabled"       => true
+                ],
+                "VCPR"         => [
+                    "label"         => "Vice-President",
+                    "enabled"       => true
+                ],
+                "SLPR"         => [
+                    "label"         => "Sales person",
+                    "enabled"       => true
+                ],
+                "BRKR"         => [
+                    "label"         => "Real Estate Agent",
+                    "enabled"       => true
+                ],
+                "MKOF"         => [
+                    "label"         => "Marketing Offers",
+                    "enabled"       => true
+                ],
+                "ITSP"         => [
+                    "label"         => "Tech Support",
+                    "enabled"       => true
+                ]
+            ]
+        ];
     }
 }
--- packages/Project/deanlief/src/routes.php
+++ packages/Project/deanlief/src/routes.php
@@ -1,9 +1,7 @@
 <?php
 
-
-
-
 Route::group(['prefix' => 'deanlief'], function () {
-    Route::get("delete_listing/{listing_refid}",[Project\Flipcard\DeanliefController::class, 'delete_listing']);
-    Route::get("search_listing",[Project\Flipcard\DeanliefController::class, 'search_listing']);
+    Route::get("delete_listing/{listing_refid}",[Project\Deanlief\DeanliefController::class, 'delete_listing']);
+    Route::get("search_listing",[Project\Deanlief\DeanliefController::class, 'search_listing']);
+    Route::get("variables",[Project\Deanlief\DeanliefController::class, 'variables']);
 });
