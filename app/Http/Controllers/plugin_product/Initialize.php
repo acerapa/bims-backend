8a5fc3e8869e0e2f2ccffdbb163291d692cad421
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-04-01 19:45:44 +0800

    Product Initial Structure added

---
+++ app/Http/Controllers/plugin_product/Initialize.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\plugin_product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Initialize extends Controller
+{
+    public static function initAll() {
+        /** Check if all product has json file */
+    }
+
+    public static function initProduct($product_refid) {
+        /** Check if specific product has json file */
+    }
+}
