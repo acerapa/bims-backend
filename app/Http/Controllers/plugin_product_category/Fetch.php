51b8e2c419f469cf13e06373fb295702f324c219
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-04-01 21:02:52 +0800

    Initial Structure

---
+++ app/Http/Controllers/plugin_product_category/Fetch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\plugin_product_category;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Fetch extends Controller
+{
+    public static function all() {
+
+    }
+
+    public static function subcategory($category_refid) {
+        
+    }
+}
---
+++ app/Http/Controllers/plugin_store/Initialize.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\plugin_store;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Initialize extends Controller
+{
+    //
+}
