f3fc33da68de986052d5239d4a0cb2872dd779a6
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-04-02 22:01:03 +0800

    OK

--- packages/Project/cims/src/routes.php
+++ packages/Project/cims/src/routes.php
@@ -1,10 +1,11 @@
 <?php
 
 /**
- * 
- * cms/variables
+ * cims/auth_brgy?email=&password=&city_code=&brgy_code=&device=&datetime
+ * cims/variables
  */
 
-Route::group(['prefix' => 'cms'], function () {
+Route::group(['prefix' => 'cims'], function () {
+    Route::get("auth_brgy",[Project\CIMS\Authentication::class, 'barangayHall']);
     Route::get("variables",[Project\CIMS\CIMSController::class, 'variables']);
 });
