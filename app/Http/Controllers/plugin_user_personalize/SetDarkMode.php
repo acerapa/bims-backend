3ca06ef74153aa438eafdc1594964b95b08cf319
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-05-09 23:35:51 +0800

    User Personalize Folder Added and Initial File

---
+++ app/Http/Controllers/plugin_user_personalize/SetDarkMode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\plugin_user_personalize;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SetDarkMode extends Controller
+{
+    //
+}
