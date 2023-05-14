bfb4ac1b6ef49f03ecb64b5370368c7a5654828e
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-05-14 11:05:30 +0800

    Unused package removed

--- app/Http/Controllers/plugin_paynamics/PaymentSale.php
+++ app/Http/Controllers/plugin_paynamics/PaymentSale.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\plugin_paynamics;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Artisaninweb\SoapWrapper\SoapWrapper;
-use App\Soap\Request\GetConversionAmount;
-use App\Soap\Response\GetConversionAmountResponse;
+
 
 /**
  * https://github.com/notfalsedev/laravel-soap
@@ -15,8 +13,6 @@ use App\Soap\Response\GetConversionAmountResponse;
 class PaymentSale extends Controller
 {
     public static function post(Request $request) {
-        $init   = ini_set("soap.wsdl_cache_enabled", 0);
-        $client = new SoapClient("https://testpti.payserv.net/Paygate/ccservice.asmx?WSDL");
-        return $client;
+        return null;
     }
 }
