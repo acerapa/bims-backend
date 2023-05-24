f6e3ae95b1a1d084b3216a4815c350f0155dc92e
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2023-05-24 12:12:11 +0800

    OK

--- app/Http/Controllers/plugin_order_item/Structure.sql
+++ app/Http/Controllers/plugin_order_item/Structure.sql
@@ -8,36 +8,21 @@
           3: Seen by store
           4: Accepted by store
           5: Refused by store
+        
+        plugin_order_item.add_ons_array
+          [
+            {
+              name: '',
+              price: '',
+              photo: []
+            },
+            {
+              name: '',
+              price: '',
+              photo: []
+            }
+          ]
 */
-
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: May 08, 2023 at 06:50 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.3.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `foxc_shop_national`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `plugin_order_item`
---
 
 CREATE TABLE `plugin_order_item` (
   `dataid` int(10) NOT NULL,
@@ -57,28 +42,6 @@ CREATE TABLE `plugin_order_item` (
   `status` int(3) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+ALTER TABLE `plugin_order_item` ADD PRIMARY KEY (`dataid`), ADD UNIQUE KEY `reference_id` (`reference_id`);
+ALTER TABLE `plugin_order_item` MODIFY `dataid` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- Indexes for table `plugin_order_item`
---
-ALTER TABLE `plugin_order_item`
-  ADD PRIMARY KEY (`dataid`),
-  ADD UNIQUE KEY `reference_id` (`reference_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `plugin_order_item`
---
-ALTER TABLE `plugin_order_item`
-  MODIFY `dataid` int(10) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
